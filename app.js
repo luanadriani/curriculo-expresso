91d722ad6646142f892d18049141ac70f2b58713
luanadriani <luanadriani2011@gmail.com>
2019-12-16 14:45:40 -0300

    feat: add ejs in the project

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express')
+const path = require('path')
+
 const port = 3000
 const app = express()
 
-app.get('/', (nodereq, res, next) => {
-    res.send({
+// Setup view engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/', function(req, res, next) {
+    res.render('index', {
         title: "Meu primeiro servidor Express",
         version: "0.0.0"
-    });
-});
+    })
+})
 
 app.listen(port, err => {
     console.log(`Server is listening on ${port}`)
-});
+})
